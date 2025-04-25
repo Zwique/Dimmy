849af1b083671a6b0532842a50631bb83cc8bfea
Erdembileg Ariunbold <97537483+Zwique@users.noreply.github.com>
2025-04-25 15:10:49 +0900

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,5 +1,6 @@
 import pyautogui
 import time
+from pynput import mouse
 
 print("""
 DDDDD   IIIIII  MMM   MMM  MMM   MMM  Y     Y
@@ -11,28 +12,25 @@ D    D    I     M       M  M       M     Y
 DDDDD   IIIIII  M       M  M       M     Y   
 """)
 
-# Define the text and the number of times to write it
+# Get input from user
 text_to_write = input("Text to write: ")
 repeat_count = int(input("Number of times to write the text: "))
 current_count = 0
 
 def on_click(x, y, button, pressed):
+    global current_count
     if pressed:
         for _ in range(repeat_count):
             pyautogui.write(text_to_write)
             pyautogui.press('enter')
-            time.sleep(0.1)  # Small delay to ensure text is written properly
+            time.sleep(0.1)
         current_count += 1
-    
-        if current_count >= repeat_count:
-            # Close the program after printing the text repeat_count times
-            print("Closing the program ...")
-            return False  # Stop the listener and close the program
 
-# Use pyautogui's mouse event listener
-from pynput import mouse
+        if current_count >= 1:
+            print("Text writing completed. Exiting...")
+            return False
+time.sleep(5)
 
-# Create an instance of Listener and assign the on_click function to handle click events
+# Start mouse listener
 with mouse.Listener(on_click=on_click) as listener:
     listener.join()
-    
